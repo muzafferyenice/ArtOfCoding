8f7a1f9f51405cde6190d47a8126887fc5608850
YusufAtesmen <yusufatesmen@gmail.com>
2022-04-03 22:31:36 +0300

    SekerliMi

--- src/tasks/Seker.java
+++ src/tasks/Seker.java
@@ -1,2 +1,18 @@
-package tasks;public class Seker {
+package tasks;
+
+import java.util.Scanner;
+
+public class Seker {
+static  Scanner scan=new Scanner(System.in);
+
+    public Seker(String seker) {
+        if (seker.equalsIgnoreCase("EVET")) {
+            System.out.println("Kac seker ekemek istesini)");
+
+            int sekerSayisi = scan.nextInt();
+            System.out.println(sekerSayisi+" Seker ekleniyor");
+        }
+        if (seker.equalsIgnoreCase("HAYIR")) {
+        }
+    }
 }
--- src/tasks/SekerliMi.java
+++ src/tasks/SekerliMi.java
@@ -3,22 +3,28 @@ package tasks;
 import java.util.Scanner;
 
 public class SekerliMi {
-    ublic static void main(String[] args) {
+    public static void main(String[] args) {
 
-        SekerliMi sekerMi = new SekerliMi();{
 
-          Scanner scan = new Scanner(System.in);
 
-          System.out.println("Seker eklemek ister misiziniz (Evet veya Hayir yazınız)");
-            String seker = scan.nextLine().toUpperCase();
-          if (seker.equalsIgnoreCase("EVET")) {
-                System.out.println("Kac seker ekemek istesini)");
-                int sekerSayisi = scan.nextInt();
-                System.out.println("Seker ekleniyor");
-            }
-          if (seker.equalsIgnoreCase("HAYIR")) {
-            }
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Seker eklemek ister misiziniz (Evet veya Hayir yazınız)");
+        String seker = scan.nextLine();
+        Seker aa=new Seker(seker);
+
+        if (seker.equalsIgnoreCase("EVET")) {
+            System.out.println("Kac seker ekemek istesini)");
+
+            int sekerSayisi = scan.nextInt();
+            System.out.println(sekerSayisi+" Seker ekleniyor");
+        }
+        if (seker.equalsIgnoreCase("HAYIR")) {
         }
     }
-}
-}
+
+
+    }
+
+
