82a10ecd288b996d4e55c01a7ae73262834e1cbd
myildirimcakar505@gmail.com <97838757+ARO65@users.noreply.github.com>
2022-04-04 14:37:35 +0300

    04.04.2022

---
+++ src/ortak/KahveMakınası.java
@@ -0,0 +1,29 @@
+package ortak;
+
+public class KahveMakınası {
+String hangiKahve;
+
+
+    public KahveMakınası() {
+        String kCesidi1 = "turkKahvesi";
+        String kCesidi2 = "filtrekahve";
+        String kCesidi3 = "espresso";
+
+        if (kCesidi1.equals("turkKahvesi")) {
+            System.out.println(" Türk kahvesi hazırlanıyor.");
+
+
+        } else if (kCesidi2.equals("filtre_kahve")) {
+            System.out.println("Filtre kahve hazırlanıyor");
+        } else if (kCesidi3.equals("espresso")) {
+            System.out.println("Filtre kahve hazırlanıyor.");
+        } else System.out.println("Hatalı tuşlama yaptınız");
+
+
+    }
+    }
+
+
+
+
+
