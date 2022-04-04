f83b5c8b21e0b42afe8be6252087773eb9e5e5d8
myildirimcakar505@gmail.com <97838757+ARO65@users.noreply.github.com>
2022-04-04 15:07:46 +0300

    04.04.2022

---
+++ src/tasks/KahveMakınasıRunner1.java
@@ -0,0 +1,13 @@
+package tasks;
+
+import ortak.KahveMakınası;
+
+public class KahveMakınasıRunner1 {
+    public static void main(String[] args) {
+        KahveMakınası robot=new KahveMakınası();
+
+
+        System.out.println();
+
+    }
+}
