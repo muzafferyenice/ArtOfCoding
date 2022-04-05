7f443ab6fd0da43ed993825509eaa461c7814d93
YusufAtesmen <yusufatesmen@gmail.com>
2022-04-05 16:18:03 +0300

    Kahve Proje son

--- src/tasks/KahveRunner.java
+++ src/tasks/KahveRunner.java
@@ -1,13 +1,15 @@
 package tasks;
 
+import java.util.Scanner;
+
 public class KahveRunner {
     public static void main(String[] args) {
 
-    //KahveCesitleri kahve =new KahveCesitleri();
 
-    KahveCesitleri kahve =new KahveCesitleri("TURK KAHVESI ","FILTRE KAHVE "," ESPRESSO ");
-
-
+        KahveCesitleri kahve = new KahveCesitleri("TURK KAHVESI ", "FILTRE KAHVE ", " ESPRESSO ");
+        KahveCesitleri sut = new KahveCesitleri("Evet","Hayır");
+        KahveCesitleri sekerMi = new KahveCesitleri("Evet ,Hayır");
+        KahveCesitleri boyut = new KahveCesitleri("Büyük boy","Orta boy","Kucuk boy","Double boy");
 
     }
 }
